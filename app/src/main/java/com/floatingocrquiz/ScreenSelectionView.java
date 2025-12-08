99a42c964a9c9c0ea1e16479494a5828f9d3e038
moso <moso@hogtty.com>
2025-12-08 23:03:07 +0800

    Fix compilation error: Add missing RectF import

--- app/src/main/java/com/floatingocrquiz/ScreenSelectionView.java
+++ app/src/main/java/com/floatingocrquiz/ScreenSelectionView.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
